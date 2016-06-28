5075a5c438c6db7cf597fd3f841a131857afe0d3
Tyler Morrison <tym856@utulsa.edu>
2016-06-28 11:20:42 -0400

    Added the analyze.py. A simple script for doing statistical analysis on a single data set, as we did in the first tutorial.

---
+++ analyze.py
@@ -0,0 +1,20 @@
+#Python script for tutorial class
+print("Running analysis...\n");
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import data 
+patient_data=np.loadtxt('data/inflammation-01.csv',delimiter=',');
+
+
+#calculate wholistic statistics
+mean=patient_data.mean();
+max=patient_data.max();
+std_dev=patient_data.std();
+
+#plot mean over time
+mean_over_time=np.mean(patient_data, axis=0);
+
+plt.plot(mean_over_time);
+plt.show()
+
