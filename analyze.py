857edf5a7e7c9829fb9dbafd696a01f0ca6c0086
Tyler Morrison <tym856@utulsa.edu>
2016-06-28 11:25:46 -0400

    Added the printout of the calculated values

--- analyze.py
+++ analyze.py
@@ -11,6 +11,8 @@ patient_data=np.loadtxt('data/inflammation-01.csv',delimiter=',');
 mean=patient_data.mean();
 max=patient_data.max();
 std_dev=patient_data.std();
+print("mean    max  std. dev:")
+print(mean,max,std_dev)
 
 #plot mean over time
 mean_over_time=np.mean(patient_data, axis=0);
