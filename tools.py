a4e65313854a30b92b94b2c8176279ae60a82110
Tyler Morrison <tym856@utulsa.edu>
2016-06-28 14:30:13 -0400

    see previous commit

---
+++ tools.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def analyze_csv(filename):
+	"""
+	Reads the file "filename" and prints the mean, max, and standard deviation
+	for the data in "filename"
+	"""
+	data=np.loadtxt(filename,delimiter=',');
+	mean=data.mean();
+	max=data.max();
+	std_dev=data.std();
+	print("mean:",mean);
+	print("max:",max);
+	print("std. dev",std_dev);
