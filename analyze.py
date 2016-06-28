ec6ed5060fe1d493456999a9a74af70c8e2c7028
Tyler Morrison <tym856@utulsa.edu>
2016-06-28 13:42:36 -0400

    added expaned functionality to the analysis script to take all files in data labeled inflammation-*csv and output to figures

--- analyze.py
+++ analyze.py
@@ -2,21 +2,33 @@
 print("Running analysis...\n");
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
-#import data 
-patient_data=np.loadtxt('data/inflammation-01.csv',delimiter=',');
+#import file names of matching type
+filenames=glob.glob('data\inflammation-*csv');
+for file in filenames:
+	print("\n",file[5:-3])
+	#import data 
+	patient_data=np.loadtxt(file,delimiter=',');
 
 
-#calculate wholistic statistics
-mean=patient_data.mean();
-max=patient_data.max();
-std_dev=patient_data.std();
-print("mean    max  std. dev:")
-print(mean,max,std_dev)
+	#calculate wholistic statistics
+	mean=patient_data.mean();
+	max=patient_data.max();
+	std_dev=patient_data.std();
+	print("mean:",mean);
+	print("max:",max);
+	print("std. dev",std_dev);
 
-#plot mean over time
-mean_over_time=np.mean(patient_data, axis=0);
 
-plt.plot(mean_over_time);
-plt.savefig("inflammation-01.png");
+	#plot mean over time
+	mean_over_time=np.mean(patient_data, axis=0);
+	
+	plt.figure(file)
+	plt.title(file[5:-3])
+	plt.xlabel("day");
+	plt.ylabel("inflammation level")
+	plt.plot(mean_over_time);
+	plt.savefig("figures\\"+file[5:-3]+"png");
+	plt.close(file)
 
